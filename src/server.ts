b05150836d29e58ac6e7d910fa7120ab28c8706a
Nathan <nathanwilliams1149@gmail.com>
2025-04-28 04:03:31 -0700

    i

--- src/server.ts
+++ src/server.ts
@@ -4,7 +4,7 @@ import connectDB from "./db";
 const PORT = process.env.PORT || 5000;
 
 connectDB().then(() => {
-app.listen(PORT, () => {
-console.log(Server running on port ${PORT});
-});
+  app.listen(PORT, () => {
+    console.log(`Server running on port ${PORT}`);
+  });
 });
